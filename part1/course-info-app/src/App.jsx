d3f10440a95ca3b9e414e8be18f57c65c0327170
NotTheRealRohit <iam.rohitsaji@gmail.com>
2024-03-02 20:51:18 +0530

    Refactor for exercise 1.5

--- part1/course-info-app/src/App.jsx
+++ part1/course-info-app/src/App.jsx
@@ -32,27 +32,29 @@ const Footer = (props) => <p>Number of exercises {props.items[0].exercises + pro
 // }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-     <Header name={course} />
-     <Content items={parts} />
-     <Footer items={parts} />
+     <Header name={course.name} />
+     <Content items={course.parts} />
+     <Footer items={course.parts} />
      {/* <Practice /> */}
     </>
   )
