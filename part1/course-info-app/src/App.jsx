476603876070c6eff14c0ce2d575875dafeab873
NotTheRealRohit <iam.rohitsaji@gmail.com>
2024-03-02 20:48:27 +0530

    refactor for 1.3 and 1.4

--- part1/course-info-app/src/App.jsx
+++ part1/course-info-app/src/App.jsx
@@ -3,9 +3,9 @@ const Header = (props) => <h1>{props.name}</h1>
 const Content = (props) => {
   return (
     <div>
-      <InnerContent part={props.items[0].part} exercises={props.items[0].exercises}/>
-      <InnerContent part={props.items[1].part} exercises={props.items[1].exercises}/>
-      <InnerContent part={props.items[2].part} exercises={props.items[2].exercises}/>
+      <InnerContent part={props.items[0].name} exercises={props.items[0].exercises}/>
+      <InnerContent part={props.items[1].name} exercises={props.items[1].exercises}/>
+      <InnerContent part={props.items[2].name} exercises={props.items[2].exercises}/>
     </div>
   )
 }
@@ -21,19 +21,29 @@ const InnerContent = (props) => {
 const Footer = (props) => <p>Number of exercises {props.items[0].exercises + props.items[1].exercises + props.items[2].exercises}</p>
 
 
+// const Practice = () => {
+//   const arr1 = [1,2,3,4]
+//   const arr2 = arr1.map(item => item + 1)
+//   const [...rest] = arr1;
+//   console.log(arr2,rest)
+// return (
+//   <></>
+// )
+// }
+
 const App = () => {
   const course = 'Half Stack application development'
-  const partAndExercises = [
+  const parts = [
     {
-      part: 'Fundamentals of React',
+      name: 'Fundamentals of React',
       exercises: 10
     },
     {
-      part: 'Using props to pass data',
+      name: 'Using props to pass data',
       exercises: 7
     },
     {
-      part: 'State of a component',
+      name: 'State of a component',
       exercises: 14
     }
   ]
@@ -41,8 +51,9 @@ const App = () => {
   return (
     <>
      <Header name={course} />
-     <Content items={partAndExercises} />
-     <Footer items={partAndExercises} />
+     <Content items={parts} />
+     <Footer items={parts} />
+     {/* <Practice /> */}
     </>
   )
 }
