e867f856dec24f0380b7190543880e9736db1742
NotTheRealRohit <iam.rohitsaji@gmail.com>
2024-03-03 19:06:18 +0530

    adding practise-dir to track practises

---
+++ practice-dir/src/App.jsx
@@ -0,0 +1,104 @@
+// import { useState } from 'react';
+
+import { useState } from "react";
+
+// const Display = ({counter}) => <p>{counter}</p>;
+  
+
+// const Button = ({onSmash, text}) => <button onClick={onSmash}> {text}</button>;
+
+
+// const App = () => {
+//   // const [counter, setCounter] = useState(10);
+//   const [clicks, setClick] = useState({
+//     left: 0,
+//     right: 0
+//   });
+//   const [trackClicks, setTrack] = useState([]);
+//   const [totalClicks,setTotal] = useState(0);
+
+//   // console.log("Counter set to ::"+counter);
+//   console.log("Clicks set to ::",clicks);
+//   console.log("Track set to ::"+trackClicks);
+//   console.log("Total set to ::"+totalClicks);
+
+//   // const increaseByOne = () => {
+//   //   console.log("Increasing, before :: "+counter);
+//   //   setCounter(counter + 1);
+//   // }
+
+//   // const setToZero = () => {
+//   //   console.log("zeroing, before :: "+counter);
+//   //   setCounter(0);
+//   // }
+
+//   const incrementLeftClick = () => {
+//     console.log("left, before :: "+clicks.left);
+//     setTrack(trackClicks.concat('L'));
+//     const updateLeftClick = clicks.left + 1;
+//     console.log("left click before set::"+clicks.left);
+//     setClick({
+//       ...clicks,
+//       left: updateLeftClick
+//     });
+//     console.log("left click after set::"+clicks.left);
+//     setTotal(updateLeftClick + clicks.right);
+//   }
+
+//   const incrementRightClick = () => {
+//     console.log("right, before :: "+clicks.right);
+//     setTrack(trackClicks.concat('R'));
+//     const updateRightClick = clicks.right + 1;
+//     setClick({
+//       ...clicks,
+//       right: updateRightClick
+//     });
+//     setTotal(clicks.left + updateRightClick);
+//   }
+
+//   return (
+//     <div>
+//       {/* <Display counter={counter}/>
+//       <Button onSmash={increaseByOne} text="Click Me"/>
+//       <Button onSmash={setToZero} text="Dont Click Me"/> */}
+
+//       <div>
+//         {clicks.left}
+//         <Button onSmash={incrementLeftClick} text="Left"/>
+//       </div>
+//       <div>
+//         {clicks.right}
+//         <Button onSmash={incrementRightClick} text="Right"/>
+//       </div>
+//       <p>
+//         {trackClicks.join(' ')}
+//       </p>
+//       <p>
+//         {totalClicks}
+//       </p>
+//     </div>
+//   )
+// }
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}> {text}</button>
+
+
+const Display = ({counter}) => <p>{counter}</p>
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const updateCounter = (value) => {
+    console.log("Counter set to ::"+value);
+    setCounter(value);
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={() => updateCounter(1000)} text="Set Counter to 1000" />
+      <Button handleClick={() => updateCounter(counter + 1)} text="Increment counter" />
+    </div>
+  )
+}
+
+export default App
